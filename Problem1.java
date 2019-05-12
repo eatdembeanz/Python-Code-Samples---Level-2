6a03ea7b73b9049c5afbf9cb066dd82371806a98
eatdembeanz <eathembeans@gmail.com>
2019-05-12 16:28:14 -0500

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,12 @@
+	//Benjamin Page 5/7/2019
+	/*Module 5 Problem 1
+		prints Hello World*/
+class HelloWorld
+{
+public static void main(String[]args)
+{
+
+System.out.println("Hello World");
+}
+	
+}
