ec349eb190c97855a6dd497ff102e9db26ab660b
eatdembeanz <eathembeans@gmail.com>
2019-05-26 21:15:41 -0500

    Add files via upload

---
+++ W7P1.java
@@ -0,0 +1,14 @@
+// Benjamin Page
+//5/21/2019
+
+//Problem -- Using the following code sample, modifies the attribute x and accesses and prints the object
+
+public class sampleClass {
+  int x;
+
+  public static void main(String[] args) {
+    sampleClass myObj = new sampleClass();
+	myObj.x = 20;
+	System.out.println(myObj.x);
+  }
+}
