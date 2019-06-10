32f1458f28caf1cf8f8020096c613f0917cc93f2
eatdembeanz <eathembeans@gmail.com>
2019-06-09 19:57:12 -0500

    Add files via upload

---
+++ Week 9 Problem 1.java
@@ -0,0 +1,33 @@
+//Benjamin Page
+//6/4/2019
+//Problem 1-- Extends a code sample by adding another for dogs.
+
+class Animal{
+public void animalSound(){
+System.out.println("The animal makes a sound");
+}
+}
+
+class Pig extends Animal
+{
+public void animalSound()
+{
+System.out.println("The pig oink");
+}
+}
+
+class Dog extends Animal
+{
+public void animalSound()
+{
+System.out.println("The dog barks");
+}
+}
+
+class Cat extends Animal
+{
+public void animalSound()
+{
+System.out.println("The cat meows.")
+}
+}
